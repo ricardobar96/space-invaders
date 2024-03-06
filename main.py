1d6a3d282195653bae0a8f0af444b7224f1c996e
ricardobar96 <ricardobar96@gmail.com>
2024-03-06 17:45:45 +0000

    Changed title and icon

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
+pygame.display.set_caption('Space Invaders, by Ricardo Baloira')
+icon = pygame.image.load('space-invaders-icon.png')
+pygame.display.set_icon(icon)
+
 running = True
 while running:
     for event in pygame.event.get():
