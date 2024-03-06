3f22a9458c767225c6e0083cd2ed04baafc84eb7
ricardobar96 <ricardobar96@gmail.com>
2024-03-06 18:07:56 +0000

    Positioned player at the bottom of the screen, changed icon

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption('Space Invaders, by Ricardo Baloira')
-icon = pygame.image.load('space-invaders-icon.png')
+icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
+
+player_img = pygame.image.load('player.png')
+player_x = 368
+player_y = 526
+
+def player():
+    screen.blit(player_img, (player_x, player_y))
 
 running = True
 while running:
+
+    screen.fill((98, 186, 89))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((98, 186, 89))
+    player()
+
     pygame.display.update()
